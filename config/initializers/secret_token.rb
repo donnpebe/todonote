e0401394150fb9a31cadaac12de26d26ebe61662
Donn Pebe <donny.prasetyobudi@gmail.com>
2012-09-22 15:30:25 +0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todonote::Application.config.secret_token = 'fd83c266b0b96307426af6eb9d3fde59c49c5c3a11f147ccdc96c33e46842eaa86ed80e82ff81ab3bd88ce187424f1677edee9f2d756f39619a39e1ae86dd080'
